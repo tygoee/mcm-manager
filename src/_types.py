ebb9769e29d85a82e464da2310920005389ac7d3
Tygo Everts <tygoee@outlook.com>
2023-10-23 17:27:05 +0200

    Moved types in one file

--- src/_types.py
+++ src/_types.py
@@ -1,14 +1,67 @@
-from typing import Literal
-import _typecls
+"Private type classes, exports are in _types"
+
+from typing import Literal, TypedDict, NotRequired
 
 SizeSystem = list[tuple[int, str | tuple[str, str]]]
 Side = Literal['client', 'server']
-Minecraft = _typecls.Minecraft
-CFMedia = _typecls.CFMedia
-PMMedia = _typecls.PMMedia
-MRMedia = _typecls.MRMedia
-URLMedia = _typecls.URLMedia
-Media = _typecls.Media
+
+
+class Minecraft(TypedDict):
+    version: str
+    modloader: str
+
+
+class __Media(TypedDict):
+    "Private values for Media"
+    sides: list[Literal['client', 'server']]
+    slug: str
+    name: str
+    info: NotRequired[dict[str, str]]
+
+    # Download info: url, path, size
+    _dl: tuple[str, str, int]
+
+
+class CFMedia(__Media):
+    "Media for CurseForge"
+    type: Literal['cf']
+    id: int
+
+
+class PMMedia(__Media):
+    "Media for CurseForge"
+    type: Literal['pm']
+    media: str
+
+
+class MRMedia(__Media):
+    "Media for CurseForge"
+    type: Literal['mr']
+    id: str
+
+
+class URLMedia(__Media):
+    "Media for CurseForge"
+    type: Literal['url']
+    link: str
+
+
+# Dictionary with info about a mod, resourcepack or shaderpack
+Media = CFMedia | PMMedia | MRMedia | URLMedia
 MediaList = list[Media]
-Manifest = _typecls.Manifest
-Answers = _typecls.Answers
+
+
+class Manifest(TypedDict):
+    "All information of the modpack"
+    minecraft: Minecraft
+    mods: Media
+    resourcepacks: Media
+    shaderpacks: Media
+
+
+class Answers(TypedDict):
+    manifest_file: str
+    install_path: str
+    side: Literal['client', 'server']
+    install_modloader: bool
+    launcher_path: str
